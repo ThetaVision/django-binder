1e7d728ef0ea2623431f5024c4abc3ae9827c59e
cvdgrinten <coen@vd-grinten.nl>
2025-09-26 13:26:08 +0200

    Add exclude_history_fields field for binder history

--- binder/models.py
+++ binder/models.py
@@ -608,6 +608,7 @@ class BinderModel(models.Model, metaclass=BinderModelBase):
 
 	class Binder:
 		history = False
+		exclude_history_fields: list[str] = []
 
 	class Meta:
 		abstract = True
@@ -682,6 +683,10 @@ class BinderModel(models.Model, metaclass=BinderModelBase):
 
 def history_obj_post_init(sender, instance, **kwargs):
 	instance._history = instance.binder_concrete_fields_as_dict(skip_deferred_fields=True)
+	
+	excluded_fields = getattr(sender.Binder, 'exclude_history_fields', [])
+	for field_name in excluded_fields:
+		instance._history.pop(field_name, None)
 
 	if not instance.pk:
 		instance._history = {k: history.NewInstanceField for k in instance._history}
@@ -689,7 +694,12 @@ def history_obj_post_init(sender, instance, **kwargs):
 
 
 def history_obj_post_save(sender, instance, **kwargs):
+	excluded_fields = getattr(sender.Binder, 'exclude_history_fields', [])
+	
 	for field_name, new_value in instance.binder_concrete_fields_as_dict().items():
+		if field_name in excluded_fields:
+			continue
+			
 		try:
 			old_value = instance._history[field_name]
 			if old_value != new_value:
